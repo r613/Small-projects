42f3dae328f79cda0118f620e5d2f7f7c57e5d0c
MowHogz <icreatemyweb@gmail.com>
2019-09-20 02:27:26 +0300

    v2.0

--- Queenquiz.py
+++ Queenquiz.py
@@ -1,86 +1,62 @@
-brd = []
+#the queen challenge
+matrix = []
+for i in range(8):
+    matrix.append([])
+    for c in range(8):
+        matrix[i].append(0)
+def up(matrix,row,column):
+    for c in range(8):
+        if matrix[c][column] == 1:
+            return False
+    for c in range(8):
+        if matrix[row][c] ==1:
+            return False
+    return True
 
-for a in range(8):
-    minibrd  = []
-    for b in range(8):
-        minibrd.append(False)
-    brd.append(minibrd)
-for i in brd:
-    print i
-def find(brd):
+def ur(matrix,row,column):
+    r = row
+    c = column
+    while r>0 and c >0: #left up
+        r += -1
+        c += -1
+        if matrix[r][c] == 1:
+            return False
+    r = row
+    c = column
+    while r > 0 and c < 7: #up right
+        r += -1
+        c += 1
+        if matrix[r][c] == 1:
+            return False
+    c = column
+    r = column
+    while c < 7 and r < 7:#right down
+        c += 1
+        r += 1
+        if matrix[r][c] == 1:
+            return False
+    c = column
+    r = row
+    while c > 0 and r < 7:#down left 
+        r += 1
+        c += -1
+        if matrix[r][c] == 1:
+            return False
+    return True
+
+def find(matrix):
     for row in range(8):
-        for column in range(8):
-            if fits(brd,row,column):
-                
-                temp_brd = [x[:]for x in brd]
-                temp_brd[row][column] = True
-                res = find(temp_brd)
-                if res:
-                    return res
-        if empty_row(brd,row):
-            
+        if 1 not  in matrix[row]:
+            for column in range(8):
+                if ur(matrix,row,column) and up(matrix,row,column):
+                    temp = [x[:] for x in matrix]
+                    temp[row][column] = 1
+                    find(temp)
+                    
             return False
         else:
-            
             pass
-    print "\n"
-    Print(brd)
-    return False
-def fits(brd,row,column):
-    if empty_row(brd,row) and empty_column(brd,column) and empty_side(brd,row,column):
-        return True
-    else:
-        return False
-def empty_row(brd,row):
-    for i in brd[row]:
-        if i:
-            return False
-    return True
-def empty_column(brd,column):
-    for unit in brd:
-        if unit[column]:
-            return False
-    return True
-def empty_side(brd,row,column):
-    trow = row
-    tcolumn = column
-    while trow<8 and tcolumn<8:
-        if brd[trow][tcolumn]:
-            return False
-        trow += 1
-        tcolumn += 1
-    trow = row
-    tcolumn = column
-    while trow >= 0 and tcolumn< 8:
-        if brd[trow][tcolumn]:
-            return False
-        trow += -1
-        tcolumn += 1
-    trow = row 
-    tcolumn = column 
-    while trow <8 and tcolumn >=0:
-        if brd[trow][tcolumn]:
-            return False
-        trow += 1
-        tcolumn += -1
-    trow = row 
-    tcolumn = column 
-    while trow >=0 and tcolumn >=0:
-        if brd[trow][tcolumn]:
-            return False
-        trow += -1
-        tcolumn += -1
-    return True
-def Print(brd):
-    text = "\n"
-    for row in brd:
-        line = "\n"
-        for space in row:
-            line += " "
-            if space:
-                line += "X"
-            else: 
-                line += "+"
-        text += line
-    print text
-find(brd)
+    for i in matrix:
+        print (i)
+    print()
+find(matrix)
